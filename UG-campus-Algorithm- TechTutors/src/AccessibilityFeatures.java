ca26b96c0948b70dc83e26d802b8df2996fcea3f
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-25 09:02:04 -0700

    Update AccessibilityFeatures.java

--- UG-campus-Algorithm- TechTutors/src/AccessibilityFeatures.java
+++ UG-campus-Algorithm- TechTutors/src/AccessibilityFeatures.java
@@ -1,6 +1,4 @@
-;
-        locationAccessibility.put("Sarbah Park", new Accessibilifalse, false, false, false, 0, 1.0));
-        locationAccessibility.put("Fire Station"
+;n"
     public static AccessibilityRoute createAccessibleRoute(List<String> path, double distance, double time, String accessibilityType) {
         AccessibilityRoute route = new AccessibilityRoute(path, distance, time, accessibilityType);
         
