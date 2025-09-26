22a304e1f30396c293cd171fe3b789fcc5f3718b
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-26 09:24:56 -0700

    Update CriticalPath.java

--- UG-campus-Algorithm- TechTutors/src/CriticalPath.java
+++ UG-campus-Algorithm- TechTutors/src/CriticalPath.java
@@ -1,13 +1,1 @@
-k[0]);
-        Arrays.sort(ret, new Comparator<Task>() {
 
-
-                if(o1.isDependent(o2))return -1;
-                if(o2.isDependent(o1))return 1;
-                return 0;
-            }
-        });
-
-        return ret;
-    }
-}
