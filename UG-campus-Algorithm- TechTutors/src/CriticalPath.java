c86c97e1eb5f5e3c3cde9d4d940bebb74501dacb
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-26 09:24:48 -0700

    Update CriticalPath.java

--- UG-campus-Algorithm- TechTutors/src/CriticalPath.java
+++ UG-campus-Algorithm- TechTutors/src/CriticalPath.java
@@ -1,10 +1,6 @@
 k[0]);
         Arrays.sort(ret, new Comparator<Task>() {
 
-            @Override
-            public int compare(Task o1, Task o2) {
-                int i= o2.criticalCost-o1.criticalCost;
-                if(i != 0)return i;
 
                 if(o1.isDependent(o2))return -1;
                 if(o2.isDependent(o1))return 1;
