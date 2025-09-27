bda1190daf0fff70dba0ddf788fccc82a5b6d81a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-27 03:41:36 -0700

    Update TrafficSimulator.java

--- UG-campus-Algorithm- TechTutors/src/TrafficSimulator.java
+++ UG-campus-Algorithm- TechTutors/src/TrafficSimulator.java
@@ -2,7 +2,7 @@
 acFactors.put(LocalTime.of(11, 0), 1.3);
         timctors.put(LocalTime.of(16, 0), 1.6);
         timeTrafficFactors.put(LocalTime.of(17, 0), 1.9);
-        timeTrafficFactors.put(LocalTime.of(18, 0), 1.7);
+        timers.put(LocalTime.of(18, 0), 1.7);
     }
     
     public static double calculateTrafficFactor(String location, LocalTime time) {
