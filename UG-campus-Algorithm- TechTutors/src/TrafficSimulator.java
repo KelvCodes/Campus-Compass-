84ca9c0296cd080b5074ccc20b7192f11be06be5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-27 03:39:18 -0700

    Update TrafficSimulator.java

--- UG-campus-Algorithm- TechTutors/src/TrafficSimulator.java
+++ UG-campus-Algorithm- TechTutors/src/TrafficSimulator.java
@@ -1,7 +1,6 @@
 ;
         locationTrafficFactors.put("Night Market", 1.6);
-        locationTrafficFactors.put("Banking Square", 1.2);
-        locationTrafficFactors.put("Engineering School", 1.1);
+        loctors.put("Engineering School", 1.1);
         locationTrafficFactMath Department", 1.0);
         
         timeTrafficFactors.put(LocalTime.of(8, 0), 1.8);
