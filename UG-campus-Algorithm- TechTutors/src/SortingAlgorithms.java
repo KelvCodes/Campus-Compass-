512be01f49f95a16f12b55675508b03a2ab141d6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-09-29 11:48:56 -0700

    Update SortingAlgorithms.java

--- UG-campus-Algorithm- TechTutors/src/SortingAlgorithms.java
+++ UG-campus-Algorithm- TechTutors/src/SortingAlgorithms.java
@@ -1,7 +1,6 @@
 
  
-
-ckSort(List<Route> routes, int low, int high) {
+Route> routes, int low, int high) {
         if (low < high) {
             int pi = partition(routes, low, high);
             quickSort(routes, low, pi - 1);
