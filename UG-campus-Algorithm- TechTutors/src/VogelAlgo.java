0144d1ecbe75da7ed685d1f718a9be9ef86ad316
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-01 10:29:51 -0700

    Update VogelAlgo.java

--- UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
+++ UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
@@ -1,7 +1,5 @@
 
-                i
-                    md = res[0];  // max diff
-                    pm = is of min cost
+     s of min cost
                 }
             }
             return isRow ? new int[]{pm, pc, mc, md} : new int[]{pc, pm, mc, md};
