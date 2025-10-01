54a56d763c54044f9f9f182d282ea7ed5954add9
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-01 10:29:34 -0700

    Update VogelAlgo.java

--- UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
+++ UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
@@ -1,6 +1,4 @@
 
-        static int[] maxPenalty(int len1, int len2, boolean isRow) {
-            int mnue;
                 int[] res = diff(i, len2, isRow);
                 if (res[0] > md) {
                     md = res[0];  // max diff
