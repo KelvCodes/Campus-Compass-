cf3eba852a529cd4754e142b6d6c7a2516bd6c9d
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-01 10:28:52 -0700

    Update VogelAlgo.java

--- UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
+++ UG-campus-Algorithm- TechTutors/src/VogelAlgo.java
@@ -1,8 +1,6 @@
 isRow ? costs[j][i] : costs[i][j];
                 if (c < min1) {
-                    min2 = min1;
-                    min1 = c;
-                    minP = i;
+              
                 } else if (c < min2)
                     min2 = c;
             }
